0f507701d115dea99de0d90c8b246561862fa56e
reginafw <35670160+reginafw@users.noreply.github.com>
2018-12-04 15:20:08 -0800

    RW documented SerializableBuildingList.java

--- src/main/java/com/whereisevery1/database/model/SerializableBuildingList.java
+++ src/main/java/com/whereisevery1/database/model/SerializableBuildingList.java
@@ -5,22 +5,35 @@ import java.util.HashMap;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 //import lombok.Data;
 
+
 @JsonSerialize
+/** Represents a Serializable object containing all building data including 
+* Buildings, Room, Days, Times 
+*/
 public class SerializableBuildingList {
 
 	private HashMap<String, Building> buildings;
 
 	public SerializableBuildingList() {
 	}
-
+	
+	/** Creates SerializableBuildingList object with a specified HashMap containing Buildings
+	*@param buildings HashMap containing Buildings with key Strings
+	*/
 	public SerializableBuildingList(HashMap<String, Building> buildings) {
 		this.buildings = buildings;
 	}
-
+	
+	/**Gets buildings
+	*@return HashMap of Buildings
+	*/
 	public HashMap<String, Building> getBuildings() {
 		return this.buildings;
 	}
-
+	
+	/**Sets buildings 
+	*@param buildings a HashMap of Buildings
+	*/
 	public void setBuildings(HashMap<String, Building> buildings) {
 		this.buildings = buildings;
 	}
