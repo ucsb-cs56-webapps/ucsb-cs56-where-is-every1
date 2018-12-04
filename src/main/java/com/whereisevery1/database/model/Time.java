9e7ac45e9db043c6319482f3cd6dd6cdf4bbbe0d
reginafw <35670160+reginafw@users.noreply.github.com>
2018-12-04 15:15:11 -0800

    RW documented Time.java

--- src/main/java/com/whereisevery1/database/model/Time.java
+++ src/main/java/com/whereisevery1/database/model/Time.java
@@ -2,9 +2,16 @@ package com.whereisevery1.database.model;
 
 import java.text.SimpleDateFormat;
 
+/** Represents a time
+*/
 public class Time {
 	// Values entered as military time with format HHmm
+	/** Start time, entered as military time with format HHmm
+	*/
 	private int start;
+	
+	/** End time, entered as military time with format HHmm
+	*/
 	private int end;
 
 	// START - IMMUTABLE ATTRIBUTES
@@ -14,7 +21,11 @@ public class Time {
 
 	public Time() {
 	}
-
+	
+	/** Creates new Time by parsing time from string to military time
+	*@throws Exception 
+	*@param time string to be split by whitespaces and -
+	*/
 	public Time(String time) throws Exception {
 		String[] values = time.split("\\s*-\\s*");
 
@@ -25,23 +36,38 @@ public class Time {
 				.format((new SimpleDateFormat(originalTimeFormat)).parse(values[1])));
 
 	}
-
+	
+	/** Sets start
+	*@param start start time
+ 	*/
 	public void setStart(int start) {
 		this.start = start;
 	}
 
+	/** Sets end
+	*@param end end time
+	*/
 	public void setEnd(int end) {
 		this.end = end;
 	}
 
+	/** Gets start
+	*@return start time
+	*/
 	public int getStart() {
 		return this.start;
 	}
 
+	/** Gets end
+	*@return end time
+	*/
 	public int getEnd() {
 		return this.end;
 	}
-
+	
+	/** Creates message to display the class time
+	*@return message “Class starts at:__ Ends at__”
+	*/
 	public String toString() {
 		return "\nClass starts at: " + start + " Ends at " + end;
 	}
