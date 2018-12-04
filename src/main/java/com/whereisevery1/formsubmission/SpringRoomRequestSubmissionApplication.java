d6a6963446c6953832757818c04af3ff70133cdc
Andrew Price <agprice@umail.ucsb.edu>
2018-12-03 17:56:17 -0800

    AP Removed code to build json database, beacause it breaks heroku.

--- src/main/java/com/whereisevery1/formsubmission/SpringRoomRequestSubmissionApplication.java
+++ src/main/java/com/whereisevery1/formsubmission/SpringRoomRequestSubmissionApplication.java
@@ -5,7 +5,6 @@ package com.whereisevery1.formsubmission;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 import com.whereisevery1.database.model.DatabaseCreator;
 
 @SpringBootApplication
@@ -13,18 +12,19 @@ public class SpringRoomRequestSubmissionApplication {
 
 	public static void main(String[] args) {
 
+		SpringApplication.run(SpringRoomRequestSubmissionApplication.class, args);
 		if (args != null && args.length > 0) {
 			// If you provide an argument when running the code, it will build the json
 			// database. DOESN'T WORK ON HEROKU.
 			if (args[0].equals("json")) {
-				DatabaseCreator d = new DatabaseCreator();
-				d.runScrape();
-				d.writeToJSON();
+				// DatabaseCreator d = new DatabaseCreator();
+				// d.runScrape();
+				// d.writeToJSON();
 				// SpringApplication.run(SpringRoomRequestSubmissionApplication.class, args);
 			}
 		} else {
 			// Start the spring application if we're not building the database.
-			SpringApplication.run(SpringRoomRequestSubmissionApplication.class, args);
+			// SpringApplication.run(SpringRoomRequestSubmissionApplication.class, args);
 		}
 	}
 }
